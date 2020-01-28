f83f09f7462cbba5cf3a53a5cedc05f3eef0728b
Rebecca Manzi <rebeccamanzi@gmail.com>
2020-01-28 12:46:57 -0300

    Comecando a colocar pecas no tabuleiro

--- Xadrez/tabuleiro/Tabuleiro.cs
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -13,9 +13,17 @@
             this.pecas = new Peca[linhas,colunas];
         }
 
-        public Peca peca(int linha, int coluna)
+        public Peca peca(int linha, int coluna) // para poder acessar uma peça (pois a matriz pecas[,] está protegida)
         {
             return pecas[linha, coluna];
         }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p; // colocando a peça p na posição pos
+            p.posicao = pos;
+        }
+
+
     }
 }
